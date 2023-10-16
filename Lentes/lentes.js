f3b5ea9d03b90649eb759d0c735576f62eabc6ba
LautaroDellolio <lautarodellolio@gmail.com>
2023-10-16 10:41:07 -0300

    completo

---
+++ Lentes/lentes.js
@@ -0,0 +1,44 @@
+const artLentes = [
+  {
+    name: "Kit Lentes Cooke S4i T2",
+    description:
+      "18, 25, 32, 50, 75mm Cooke S4i || Montura: PL (LDS) || Apertura: T2 | T22 || Rotación foco: 270º | 300º || Diámetro frontal: 110mm || Cobertura Sensor: Super 35 || Peso: 1,5 | 4,7kg",
+    img: "",
+  },
+  {
+    name: "Kit Lentes Leica Summicron",
+    description:
+      "18, 25, 35, 50, 75, 100mm Leica Summicron C || Montura: PL || Apertura: T2.0 || Diámetro Frontal: 95mm || Rotación Anillo foco: 300º || Cobertura Sensor: Super 35 || Sin distorsión geométrica || Peso: 1.3 Kg | 1.8nKg",
+    img: "",
+  },
+  {
+    name: "Kit Lentes Rokinon T1.5 Cine DS",
+    description:
+      "24, 35, 50, 85mm Cine-Style Lentes || Formato: Full Frame || Tipo de Montura: Canon EF || Cubiertas Sensores de cuadro completo || Rango de exposición: T1.5 a T22 || Básculas laterales de enfoque y T-Stop || Anillo de apertura De-Clicked || Enfoque y apertura manual || Peso: 615 gramos || No se alquilan por separado",
+    img: "",
+  },
+  {
+    name: "Kit Lentes Schneider Xenon FF-Prime T.2.1",
+    description:
+      "25, 35, 50, 75, 100mm Schneider Xenon || Monturas: E (Sony), EF (Canon) y PL || Cobertura Full Frame: 36mm x 24mm || Diseñados para resoluciones 4K y superiores || Ligeros y compensados en su construcción || Optimización de destello y look cine || Iris de 14 hojas para un bokeh bello y suave || Marcas impresas en el cuerpo || Rosca para filtros de 95mm",
+    img: "",
+  },
+  {
+    name: "Kit Lentes Zeiss Compact Primes CP2",
+    description:
+      "Montura PL || 21mm Distagon T* || 28mm Distagon T* || 35mm Distagon T* || 50 mm Makro-Planar T* || 85 mm Planar T*",
+    img: "",
+  },
+  {
+    name: "Zoom Angenieux Rouge 30-80mm 2.8",
+    description:
+      "Tipo de Montura: Montura PL || Longitud Focal: Comparativa 30-80mm || Apertura fotométrica: T2.8 || Rango de zoom: 2.7x || Distancia mínima del objeto: 2 '(0,61 m) || Cobertura: S35 completo (diagonal de 31,1 mm) || Peso: 1,9 kilogramos",
+    img: "",
+  },
+  {
+    name: "Zoom Fujinon MK 18-55mm F2.9",
+    description:
+      " Tipo de Montura: Montura E || Longitud focal: 18-55 mm || Relación de Zoom: 3.0 x || Gear pitch (paso de engranaje) estándar de 0,8M || Número de hojas del Diafragma: 9 || Triple anillo para el enfoque, abertura y diafragma || Cambio de diafragma continuo y silencioso, sin clics || Dimensiones: 206mm x 85mm x 206mm || Peso: 980 gramos",
+    img: "",
+  },
+];
---
+++ Luces/luces.js
@@ -0,0 +1,50 @@
+const artLuces = [
+  {
+    name: "Fresnel ARRI 1000w",
+    description:
+      "Modelo: ARRI TRUE BLUE T1 1000w || Voltaje: 120 - 240v || Dim: Largo: 224 mm / Ancho: 290 mm / Alto: 427 mm || Peso: 5.2 Kilogramos || Material: Aluminio || Portalámparas: G22/38 || Montaje: 5/8” Baby Spigot || Diámetro del Vidrio: 150 mm || Cable de alimentación de 3 metros de longitud || Resistente al agua",
+    img: "",
+  },
+  {
+    name: "Hmi Par 12Kw Filmgear",
+    description:
+      "Capacidad 12.000W || Voltaje 160V || Lente Protección Diam 500mm || Peso: 50.7 kilogramos || Tipo de Lámpara: HMI 12000W SE || Portalámpara G38 || Perno (Spigot) 28mm || Luminaria Clase I IP23 || Rango de Operación 30°-0°-30° || Temperatura de Color: 5600K",
+    img: "",
+  },
+  {
+    name: "Hmi Par 6000w Arri",
+    description:
+      "Modelo: ARRISUN 60 || Dim: Prof 695 mm / Ancho 562 mm / Alto 890 mm || Peso: 32.5 kilogramos || Material: Aluminio || Tipo de Lámpara: HMI 6.000 w/se G38 || Portalámpara: G38 de alto voltaje || Montaje: Spigot 28mm || Temperatura de Color: 6000K || Diámetro del Vidrio: 420mm || Dimensión de Visera: 510mm || Dimensión del Portafiltro: 500mm",
+    img: "",
+  },
+  {
+    name: "Kino Flo Celeb 201",
+    description:
+      "Modelo: Kino Flo Celeb 201 || CRI: 95 || Conector: AC: IEC connection || Conector DC: 3-pin XLR female || Entrada universal 100VAC - 240VAC o 24VDC || Temperatura de Color: 2700-6500K || Operación silenciosa y sin parpadeos || Dim: Largo 61 cm / Ancho 35.5 cm / Alto 12.7 cm || Peso: 6.8 kilogramos",
+    img: "",
+  },
+  {
+    name: "Kit Dedolight 150w",
+    description:
+      "Potencia: 150w || Luz de tungsteno || Montaje: 5/8” stand || Peso: 1 kilogramo || Temperatura de Color: 3000 º K || Peso del Kit: 15 kilogramos (aprox.)",
+    img: "",
+  },
+  {
+    name: "Maxibrut 9 Lámparas",
+    description:
+      "Maxibrut Dexe 9 Lámparas x 1000w || Lámparas: Par 64 || Peso: 42.5 kilogramos || Montaje: Grampa C o Perno 28 mm || Cable de alimentación de 2 mts de largo",
+    img: "",
+  },
+  {
+    name: "Minibrut 6 Lámparas",
+    description:
+      "Modelo: Dexel 60-02 || Lámpara: Par 36 DWE 650w / 120v || Portalámpara: R7s || Montaje: Grampa C o Perno 28 mm || Cable de alimentación de 2 mts de largo",
+    img: "",
+  },
+  {
+    name: "Proton Par 54 Led",
+    description:
+      "Flat Par light de 54 LEDS RGB || DMX audiorítmico con display multifunción || Alta luminosidad / Bajo Consumo || Peso: 900 gramos (aprox.)",
+    img: "",
+  },
+];
